eab297b8046fff9a1e852df7545880c8449a6166
geckotang <macky1207@gmail.com>
2014-02-26 03:54:14 +0900

    シリアルポート見て、LEDを光らせる

--- ruby-to-arduino/arduino/arduino.ino
+++ ruby-to-arduino/arduino/arduino.ino
@@ -1,9 +1,19 @@
+char inByte;
+const int LED = 13;
+
 void setup(){
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
   Serial.println("setupDone"); 
 }
 
 void loop(){
-  Serial.println("loop");
-  delay(500);
+  if (Serial.available() > 0) {
+    inByte = Serial.read();
+    Serial.println(inByte);
+    digitalWrite(LED, HIGH);
+    delay(500);
+  } else {
+    digitalWrite(LED, LOW);
+  }
 }
