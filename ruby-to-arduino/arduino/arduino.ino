55a0ee3cc23523d12f31300f5b70fc132a4a1101
geckotang <macky1207@gmail.com>
2014-02-28 08:42:10 +0900

    ふぁぼられたら反応する

--- ruby-to-arduino/arduino/arduino.ino
+++ ruby-to-arduino/arduino/arduino.ino
@@ -1,4 +1,10 @@
+#include <AquesTalk.h>
+#include <Wire.h>
+AquesTalk atp;
+
 char inByte;
+String str;
+String inMsg;
 const int LED = 13;
 
 void setup(){
@@ -10,9 +16,30 @@ void setup(){
 void loop(){
   if (Serial.available() > 0) {
     inByte = Serial.read();
-    Serial.println(inByte);
-    digitalWrite(LED, HIGH);
-    delay(500);
+    // アカウント名を1文字ずつ受信するので
+    // 受信中はアカウント名を連結する
+    if(
+        (inByte >= 65 && inByte <= 90) || (inByte >= 97 && inByte <= 122) 
+        || (inByte >= 48 && inByte <= 57) || (inByte == 43)
+        || (inByte == 61) || (inByte == 63)
+    ){
+      inMsg.concat(inByte);    
+    }
+    // 受信し終わったらしゃべる
+    if (inByte == 10 || inByte == 13){
+      inByte = 0;
+      digitalWrite(LED, HIGH);
+      while(atp.IsBusy());
+      delay(1000);
+      inMsg.concat(".");
+      inMsg = "jekkota'nngu";
+      Serial.println(inMsg);
+      atp.SyntheS(inMsg);
+      Serial.println(atp.GetResp());
+      //atp.Synthe("/ni/faborare/mashita.");
+      inMsg = "";
+    }
+    digitalWrite(LED, LOW);
   } else {
     digitalWrite(LED, LOW);
   }
--- ruby-to-arduino/twitter-fav.rb
+++ ruby-to-arduino/twitter-fav.rb
@@ -7,7 +7,12 @@ Bundler.require
 
 #ファボられたらシリアルポートに0を送る
 
-sp = SerialPort.new("/dev/cu.usbmodem1411", 9600, 8, 1, SerialPort::NONE)
+port_file = "/dev/cu.usbmodem14241"
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+sp = SerialPort.new(port_file, baud_rate, data_bits, stop_bits, parity)
 
 stream_config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/config.yml'))
 
@@ -17,7 +22,10 @@ UserStream.configure do |config|
   config.oauth_token = stream_config[:oauth_token]
   config.oauth_token_secret = stream_config[:oauth_token_secret]
 end
-#
+
+pp Romaji.kana2romaji "geckotang"
+pp Romaji.romaji2kana "geckotang"
+
 client = UserStream.client
 client.user do |status|
   if status.has_key? "event"
@@ -25,7 +33,10 @@ client.user do |status|
       case status.event.to_sym
       when :favorite
         # ファボ
-        sp.puts "0"
+        pp s_name = 'glatyou'#Moji.downcase(status.source.screen_name)
+        pp s_name_hiragana = s_name.roman_to_hiragana
+        pp s_name_roman = s_name_hiragana.hiragana_to_roman
+        sp.puts Moji.downcase(status.source.screen_name)
       else
         # 実装されていないイベント
       end
