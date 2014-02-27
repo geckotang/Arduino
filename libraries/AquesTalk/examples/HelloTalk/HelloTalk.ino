60bb7e3101e3655143bfb246aec945df28b22325
geckotang <macky1207@gmail.com>
2014-02-27 22:26:17 +0900

    しゃべるサンプル追加

---
+++ libraries/AquesTalk/examples/HelloTalk/HelloTalk.ino
@@ -0,0 +1,16 @@
+#include <AquesTalk.h>
+#include <Wire.h>  // I2CライブラリAquesTalkライブラリ内部で使用するので定義必要
+
+// Hello Talk - AquesTalk pico LSIで「こんにちは」を繰り返し発声する
+
+AquesTalk atp;  //インスタンス定義 変数名は任意
+
+void setup()
+{
+}
+
+void loop()
+{
+	atp.Synthe("konnnichiwa.");  //引数に指定のテキストを発声
+}
+
